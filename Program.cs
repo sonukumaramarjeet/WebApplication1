c8abcbe3c23f0bbaba9b214aaacb9ffa4b60184f
sonukumar37.dp@gmail.com <sonukumar37.dp@gmail.com>
2020-12-21 18:02:41 +0530

    program file changes

--- Program.cs
+++ Program.cs
@@ -11,11 +11,20 @@ namespace WebApplication1
 {
 	public class Program
 	{
+		
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="args"></param>
 		public static void Main(string[] args)
 		{
 			CreateHostBuilder(args).Build().Run();
 		}
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="args"></param>
+		/// <returns></returns>
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
